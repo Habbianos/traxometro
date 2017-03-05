333c177d1e6f3f63ab217e921df1ea376d68ab35
Alisson Nunes <alynva@gmail.com>
2017-03-05 17:10:31 -0300

    Pager funcionando

--- traxometro/src/ListaSons.js
+++ traxometro/src/ListaSons.js
@@ -3,13 +3,36 @@ import './ListaSons.css';
 import ListaSonsPager from './ListaSonsPager';
 
 class ListaSons extends Component {
+	constructor(props){
+	    super(props);
+		this.state = {
+			page: 1
+		}
+	}
+
+	changePage = (dir) => {
+		if (this.state.page+dir >= 1 && this.state.page+dir <= Math.ceil(this.props.children.length / 3))
+			this.setState({
+				page: this.state.page+dir
+			});
+	}
+
 	render() {
+		let cartuchos = [];
+		for (let i = 0; i < this.props.children.length; i++) {
+			if (
+				this.props.children[i].props.children.id >= (this.state.page - 1) * 3 + 1 && 
+				this.props.children[i].props.children.id <= (this.state.page - 1) * 3 + 3
+			)
+				cartuchos.push(this.props.children[i]);
+		}
+
 		return (
 			<div className="ListaSons">
 				<ul>
-					{ this.props.children }
+					{ cartuchos }
 				</ul>
-				<ListaSonsPager />
+				<ListaSonsPager pageA={this.state.page} pageM={Math.ceil(this.props.children.length / 3)} changePage={this.changePage} />
 			</div>
 		);
 	}
--- traxometro/src/ListaSonsPager.js
+++ traxometro/src/ListaSonsPager.js
@@ -3,11 +3,30 @@ import './ListaSonsPager.css';
 
 class ListaSonsPager extends Component {
 	render() {
+	    const {
+	    	pageA,
+	    	pageM,
+	    	changePage
+	    } = this.props;
+
+	    let inputA = pageA <= 1,
+	    inputB = pageA >= pageM;
+
 		return (
 			<div className="ListaSonsPager">
-				<input className="LeftPage" type="button" disabled />
-				<span><span className="PageNow">1</span>/<span className="PageMax">X</span></span>
-				<input className="RightPage" type="button" />
+				<input
+					className="LeftPage"
+					type="button"
+					onClick={() => changePage(-1)}
+					disabled={inputA}
+				/>
+				<span><span className="PageNow">{pageA}</span>/<span className="PageMax">{pageM}</span></span>
+				<input
+					className="RightPage"
+					type="button"
+					onClick={() => changePage(1)}
+					disabled={inputB}
+				/>
 			</div>
 		);
 	}
