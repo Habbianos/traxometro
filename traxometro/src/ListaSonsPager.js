3a2582684244b74d76f816b5f5c4e256bc54222e
Alisson Nunes <alynva@gmail.com>
2017-03-05 13:15:46 -0300

    Estilização do paginador de cartuchos

--- traxometro/src/ListaSonsPager.js
+++ traxometro/src/ListaSonsPager.js
@@ -4,7 +4,11 @@ import './ListaSonsPager.css';
 class ListaSonsPager extends Component {
 	render() {
 		return (
-			<div className="ListaSonsPager"></div>
+			<div className="ListaSonsPager">
+				<input className="LeftPage" type="button" disabled />
+				<span><span className="PageNow">1</span>/<span className="PageMax">X</span></span>
+				<input className="RightPage" type="button" />
+			</div>
 		);
 	}
 }
