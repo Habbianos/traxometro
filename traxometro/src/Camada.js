966f2c80060c0e8db00e3d7aaf16c171fa737a7f
Alisson Nunes <alynva@gmail.com>
2017-03-06 22:31:43 -0300

    Adicionado as camadas da linha do tempo

---
+++ traxometro/src/Camada.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Camada.css';
+
+export default class Camada extends Component {
+	render() {
+		return (
+			<div className="Camada">
+			</div>
+		);
+	}
+}
