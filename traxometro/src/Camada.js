3da51630f198472a84de14e0906590058d80c398
Alisson Nunes <alynva@gmail.com>
2017-03-12 11:52:40 -0300

    Resolvido as pontes entre módulos

--- traxometro/src/Camada.js
+++ traxometro/src/Camada.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './Camada.css';
+import './modulos-e-cores.css';
 
 export default class Camada extends Component {
 	render() {
 		return (
 			<div className="Camada">
+				<i className="modulo" data-moduloClasse="1" data-moduloCor="1"/>
+				<i className="modulo" data-moduloClasse="1" data-moduloCor="1"/>
+				<i className="modulo" data-moduloClasse="2" data-moduloCor="1"/>
+				<i className="modulo" data-moduloClasse="2" data-moduloCor="2"/>
+				<i className="modulo" data-moduloClasse="3" data-moduloCor="2"/>
 			</div>
 		);
 	}
