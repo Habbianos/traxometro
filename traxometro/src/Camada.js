ffd4c5cd89d67e808a00b0e20109612e68b15577
Alisson Nunes <alynva@gmail.com>
2017-03-19 19:31:12 -0300

    Contador de camadas

--- traxometro/src/Camada.js
+++ traxometro/src/Camada.js
@@ -6,11 +6,7 @@ export default class Camada extends Component {
 	render() {
 		return (
 			<div className="Camada">
-				<i className="modulo" data-moduloClasse="1" data-moduloCor="1"/>
-				<i className="modulo" data-moduloClasse="1" data-moduloCor="1"/>
-				<i className="modulo" data-moduloClasse="2" data-moduloCor="1"/>
-				<i className="modulo" data-moduloClasse="2" data-moduloCor="2"/>
-				<i className="modulo" data-moduloClasse="3" data-moduloCor="2"/>
+				{ this.props.children }
 			</div>
 		);
 	}
--- traxometro/src/LinhaDoTempo.js
+++ traxometro/src/LinhaDoTempo.js
@@ -7,7 +7,32 @@ export default class LinhaDoTempo extends Component {
 	render() {
 		return (
 			<div className="LinhaDoTempo">
-				<Camada />
+				<Camada>
+					<i className="modulo" data-moduloClasse="1" data-moduloCor="1"/>
+					<i className="modulo" />
+					<i className="modulo" data-moduloClasse="1" data-moduloCor="1"/>
+					<i className="modulo" data-moduloClasse="2" data-moduloCor="1"/>
+					<i className="modulo" data-moduloClasse="2" data-moduloCor="2"/>
+					<i className="modulo" data-moduloClasse="3" data-moduloCor="2"/>
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+					<i className="modulo" />
+				</Camada>
 				<Camada />
 				<Camada />
 				<Camada />
