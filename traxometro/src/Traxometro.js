130605d9444663cab2cde141da38014b30ff3ebe
Alisson Nunes <alynva@gmail.com>
2017-03-19 19:50:24 -0300

    Novo app principal

--- traxometro/src/Traxometro.js
+++ traxometro/src/Traxometro.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import './Traxometro.css';
-import ListaSons from './ListaSons';
-import ListaSonsLinha from './ListaSonsLinha';
-import PalhetaSons from './PalhetaSons';
-import Reprodutor from './Reprodutor';
+import Principal from './Principal'
+import MudarLista from './MudarLista'
 
 class Traxometro extends Component {
 	constructor(props){
@@ -15,67 +13,27 @@ class Traxometro extends Component {
 		});
 
 		this.state = {
-			dbCartuchos: DbCartuchos,
-			cartuInseridos: [],
-			qtdPalhetas: 4
+			pagina: "mudarLista",
+			dbCartuchos: DbCartuchos
 		};
+
+		this.paginas = {
+			"principal": <Principal mudarPagina={this.mudarPagina} />,
+			"mudarLista": <MudarLista mudarPagina={this.mudarPagina} />
+		}
 	}
 
-	InserirCartucho = (id) => {
-		this.state.dbCartuchos.forEach(cartucho => {
-			if (cartucho.id === id) {
-				for (let i = 0; i < this.state.qtdPalhetas; i++)
-					if (this.state.cartuInseridos[i] === undefined || this.state.cartuInseridos[i] === null) {
-						let array = this.state.cartuInseridos.slice();
-						cartucho.usado = true;
-						array[i] = cartucho;
-						this.setState({
-							cartuInseridos: array
-						});
-						return;
-					}
-			}
-		})
-	}
-	RemoverCartucho = (id) => {
-		let array = this.state.cartuInseridos.slice();
-		this.state.dbCartuchos.forEach(cartucho => {
-			if (cartucho === array[id])
-				cartucho.usado = false;
-		});
-		array[id] = null;
+	mudarPagina = (nova_pagina) => {
 		this.setState({
-			cartuInseridos: array
-		});
+			pagina: nova_pagina
+		})
 	}
 
 	render() {
-
-
-		// let CartuchosInseridos = [];
-		// CartuchosInseridos[0] = DbCartuchos[0];
-		let Palhetas = [];
-		for (let i = 0; i < this.state.qtdPalhetas; i++)
-			Palhetas.push((<PalhetaSons key={i} cor={i+1} removerCartucho={this.RemoverCartucho} idPalheta={i}>{this.state.cartuInseridos[i]}</PalhetaSons>));
-
-		let ListaCartuchos = [];
-		this.state.dbCartuchos.forEach(cartucho => {
-			if (!cartucho.usado)
-				ListaCartuchos.push(
-					<ListaSonsLinha key={cartucho.id} inserirCartucho={this.InserirCartucho} idCartucho={cartucho.id}>{cartucho}</ListaSonsLinha>
-				);
-		});
-
-
 		return (
-			<div className="Traxometro">
-				<ListaSons>
-					{ListaCartuchos}
-				</ListaSons>
-				<div className="palhetas-de-sons">
-					{ Palhetas }
-				</div>
-				<Reprodutor />
+			<div className="Traxometro" onCopy={() => alert('O código da música foi copiado.')} onPaste={() => confirm('Código Trax reconhecido, deseja substituir a música atual?')}>
+				<audio></audio>
+				{ this.paginas[this.state.pagina] }
 			</div>
 		);
 	}
