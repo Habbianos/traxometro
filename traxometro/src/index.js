19301cf89e1204b5286cc3d52913ccc137a238f0
Alisson Nunes <alynva@gmail.com>
2017-03-19 23:54:52 -0300

    Updates

--- traxometro/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Traxometro from './Traxometro';
-import './index.css';
-
-ReactDOM.render(
-	<Traxometro />,
-	document.getElementById('root')
-);
