dd265d47ec3b275aab2e6d30829421b9d6212111
Alisson Nunes <alynva@gmail.com>
2017-03-24 09:29:57 -0300

    Adicionado a função que gera o código Trax

--- traxometro/src/CriadorMusica/LinhaDoTempo.js
+++ traxometro/src/CriadorMusica/LinhaDoTempo.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './LinhaDoTempo.css';
 import Camada from './Camada';
-import Modulo from './modulo'
+import Modulo from './Modulo'
 import Agulha from './Agulha';
 
 export default class LinhaDoTempo extends Component {
@@ -20,9 +20,41 @@ export default class LinhaDoTempo extends Component {
 		return saida
 	}
 
+	pegarCodigoMusica = () => {
+		let codigo = '',
+			camadas = document.querySelectorAll('.Camada')
+
+		for (let i = 0; i < camadas.length; i++) {
+			codigo += (i + 1)+':'
+			let codTemp = [],
+				modulos = camadas[i].getElementsByClassName('modulo'),
+				ultimo = 0,
+				cont = 0
+
+			for (let j = 0; j < modulos.length; j++) {
+				if (Number(modulos[j].getAttribute('data-somId')) === ultimo) {
+					cont++
+					continue
+				}
+
+				if (j) codTemp.push(ultimo+','+cont)
+				
+				ultimo = Number(modulos[j].getAttribute('data-somId'))
+				cont = 1
+			}
+			if (ultimo)
+				codTemp.push(ultimo+','+cont)
+
+			codigo += codTemp.join(';')
+
+			codigo += ':'
+		}
+		console.log(codigo)
+	}
+
 	render() {
 		return (
-			<div className="LinhaDoTempo">
+			<div className="LinhaDoTempo" onClick={this.pegarCodigoMusica}>
 				<Camada>
 					{ this.pegarRecipienteModulo(24) }
 				</Camada>
