e903dfbb20247f2a1129adca13048f05d53ec629
Alisson Nunes <alynva@gmail.com>
2018-02-25 18:05:52 -0300

    Definido posição da agulha para 0

--- traxometro/src/CriadorMusica/Agulha.js
+++ traxometro/src/CriadorMusica/Agulha.js
@@ -4,7 +4,7 @@ import './Agulha.css';
 export default class Agulha extends Component {
 	render() {
 		return (
-			<input type="range" className="Agulha" min="0" max="23" />
+			<input type="range" className="Agulha" min="0" max="23" value="0" />
 		)
 	}
 }
