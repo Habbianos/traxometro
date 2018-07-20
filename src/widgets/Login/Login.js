a8b340ec8e795f7cef4c70094a0e190209b4dd36
Alisson Nunes <alynva@gmail.com>
2018-07-20 02:40:26 -0300

    Adicionado Habbo Theme Song na tela inicial

--- src/widgets/Login/Login.js
+++ src/widgets/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./Login.css";
 import Televisor from './Televisor/Televisor';
+import Musica from './../Musica/Musica';
 import PrimeiraVez from "./PrimeiraVez/PrimeiraVez";
 import Conectar from "./Conectar/Conectar";
 
@@ -9,6 +10,7 @@ export default class Login extends Component {
 		return (
 			<div className="Login">
 				<Televisor>
+					<Musica src="./audios/habbo_theme_song.mp3" />
 					<PrimeiraVez />
 					<Conectar />
 				</Televisor>
---
+++ src/widgets/Musica/Musica.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import "./Musica.css";
+
+export default class Musica extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			src: this.props.src,
+			vol: 0.5
+		}
+
+		this.reprodutor = new Audio();
+	}
+
+	componentWillMount() {
+		this.reprodutor.preload = true;
+		this.reprodutor.src = this.state.src;
+	}
+	componentDidMount() {
+		this.reprodutor.autoplay = true;
+		this.reprodutor.loop = true;
+		this.reprodutor.volume = this.state.vol;
+	}
+	componentDidUpdate() {
+		this.reprodutor.volume = this.state.vol;
+	}
+
+	play() {
+		this.reprodutor.play();
+	}
+	pause() {
+		this.reprodutor.pause();
+	}
+	mudaMute() {
+		if (this.state.vol > 0) {
+			this.setState({vol: 0});
+		} else {
+			this.setState({vol: 0.5});
+		}
+	}
+
+	handleInputChange(event) {
+		this.setState({vol: Number(event.target.value)})
+	}
+
+	render() {
+		return (
+			<div className="Musica">
+				<button onClick={ this.mudaMute.bind(this) } className={ this.state.vol > 0.9 ? 'max' : this.state.vol < 0.05 ? 'min' : '' }>Mute</button>
+				<div>
+					<input type="range" value={ this.state.vol } min="0" max="1" step="0.0001" onChange={this.handleInputChange.bind(this)} />
+				</div>
+			</div>
+		)
+	}
+}
