6a41905128df9727c21c654e5d0677c486097f35
Alisson Nunes <alynva@gmail.com>
2018-07-24 01:17:02 -0300

    Utilizando .env's para armazenar chaves da API do Firebase
    Utilizar a estrutura (.env.development e .env.production):
    apiKey: "<API_KEY>",
    authDomain: "<PROJECT_ID>.firebaseapp.com",
    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
    storageBucket: "<BUCKET>.appspot.com",
    messagingSenderId: "<SENDER_ID>",

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import './index.css';
 import * as firebase from 'firebase';
 
 var config = {
-	apiKey: "AIzaSyDpANc9RutVa1_ATWJ00pXK9ODvzC5UAPU",
-	authDomain: "traxometro.firebaseapp.com",
-	databaseURL: "https://traxometro.firebaseio.com",
-	projectId: "traxometro",
-	storageBucket: "traxometro.appspot.com",
-	messagingSenderId: "362805206844"
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	databaseURL: process.env.REACT_APP_databaseURL,
+	projectId: process.env.REACT_APP_projectId,
+	
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId
 };
 firebase.initializeApp(config);
 
